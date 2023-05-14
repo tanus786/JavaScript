13a1eecf86fe3650c5c457aaf3443686579710c9
Tanu soni <Tanusoni2312@gmail.com>
2023-05-14 19:43:21 +0530

    completed ufff

---
+++ index.js
@@ -0,0 +1,96 @@
+// alert("test")
+// document.write(3+4)
+// window.alert("how are you?")
+
+// document.write(prompt("Enter Name"))
+// name = prompt("Enter Name")
+// document.write(name)
+
+
+// document.write(confirm("would you like to exit"))
+// return true or false
+
+//Variables in JS
+
+// var a = 145
+// var b = 67
+// var name = "Tanu"
+// document.write("a+b=",a+b)
+// document.write("<br>")
+// document.write("a-b=",a-b)
+// document.write("<br>")
+// document.write(typeof a,"<br>")
+// document.write(typeof name)
+
+// let abc = 567
+// document.write(abc)
+
+//Keywords:
+//identifier:
+//litrals:
+// var abc = 890
+
+//Datatypes
+
+// var n1 = 56.05
+//number
+
+// document.write(typeof n1,"<br>")
+//String
+
+// var name = "test"
+// document.write(typeof name,"<br>")
+// document.write(name.length,"<br>")
+// document.write(name.toUpperCase(),"<br>")
+// document.write(name.charAt(0),"<br>")
+
+//boolean
+
+// var a = true
+// var b = false
+// document.write(a,"<br>")
+
+//object
+
+// var ob = {'name':'Tanu'}
+// document.write(typeof ob,"<br>")
+// document.write(ob['name'],"<br>")
+
+//Arrays
+
+// var nums=[1,2,3,4,5]
+// document.write(nums.length,"<br>")
+// document.write(nums[0],"<br>")
+// document.write(typeof nums,"<br>")
+
+//Control Statements
+
+// var a = parseInt( prompt("Enter number"))
+// if(a>0){
+//     document.write("+ve")
+// }
+// else{
+//     document.write("-ve")
+// }
+
+//loops
+
+// for(var i = 0;i<5;i++){
+//     document.write(i,"<br>")
+// }
+
+// var i =1
+// while( i < 5){
+//     document.write(i,"<br>")
+//     i++;
+// }
+
+//functions in js
+
+// function sum(){
+//     document.write("Hello")
+// }
+
+//calling function
+
+// sum()
